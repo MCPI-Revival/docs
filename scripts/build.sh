74fcdd638a39142741d163f98d19f21a44430677
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2025-11-30 13:57:46 -0500

    Include RakNet Docs

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,20 @@
 #!/bin/sh
 set -e
+
+# Build Site
 mkdocs build --strict
+
+# RakNet
+cd site
+tmp="$(pwd)/tmp"
+rm -rf "${tmp}"
+mkdir "${tmp}"
+name='RakNet'
+zip="${tmp}/${name}.zip"
+url='http://www.jenkinssoftware.com/raknet/downloads/RakNet_PC-4.036.zip'
+wget -O "${zip}" "${url}"
+unzip -d "${tmp}" "${zip}"
+dir="$(pwd)/${name}"
+rm -rf "${dir}"
+mv "${tmp}/Help" "${dir}"
+rm -rf "${tmp}"
