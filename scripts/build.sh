27a53c386824ea2fab7985dc913a7650d6ca6d51
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2025-11-30 14:01:47 -0500

    Quiet Build

--- scripts/build.sh
+++ scripts/build.sh
@@ -12,8 +12,8 @@ mkdir "${tmp}"
 name='RakNet'
 zip="${tmp}/${name}.zip"
 url='http://www.jenkinssoftware.com/raknet/downloads/RakNet_PC-4.036.zip'
-wget -O "${zip}" "${url}"
-unzip -d "${tmp}" "${zip}"
+wget --quiet -O "${zip}" "${url}"
+unzip -q -d "${tmp}" "${zip}"
 dir="$(pwd)/${name}"
 rm -rf "${dir}"
 mv "${tmp}/Help" "${dir}"
